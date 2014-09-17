8c02925d34067407e40351b11abe6b01de42f645
Melvin <melvin@alsu.com.do>
2014-09-17 17:10:50 -0400

    Removing roles in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#For Create Roles
-['beginner', 'intermediate', 'expert', 'admin'].each do |role|
-  Role.find_or_create_by name: role
-end
-
 Room.create!(
     id: 1000,
     level_id: 2,
